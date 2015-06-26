14b05f011779bdc4f43d1ec81aabe609f35b41c1
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-26 14:28:00 +0530

    build script updated for arm machine

--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 #Looking for the PWD Path in this machine
 path=`pwd`
+arch=$(uname -m)
 
 # building alljoyn component services
 printf "Building started for AllJoyn component services.\n"
 cd $path/components/alljoyn/core/alljoyn/services/about/
-scons WS=off BINDINGS=cpp
+if [ $arch == 'i686' ]; then
+	scons WS=off BINDINGS=cpp
+
+	if [ -f $path/components/alljoyn/core/alljoyn/services/about/build/linux/x86/debug/dist/about/bin/AboutClient ]; then
+		printf "Alljoyn About Service build successful.!\n"
+	else
+		printf "Something went wrong.!\n"
+	fi
+elif [ $arch == 'armv61' ]; then
+	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr 
+
+	if [ -f $path/components/alljoyn/core/alljoyn/services/about/build/linux/arm/debug/dist/about/bin/AboutClient ]; then
+		printf "Alljoyn About Service build successful.!\n"
+	else
+		printf "Something went wrong.!\n"
+	fi
+fi
 cd $path
-printf "Alljoyn About Service build successful.!\n"
