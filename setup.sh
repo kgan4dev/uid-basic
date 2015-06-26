83949be9a22d7ef5f31e7cd2e22cc1b6fc878547
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-26 17:57:23 +0530

    Created symbolic link for Dashboard to run under apache

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@
 # Setup for Alljoyn
 sudo apt-get update
 sudo apt-get install build-essential gcc g++ libssl-dev python scons curl apache2 php5 libapache2-mod-php5 -y
+
+# Assuming apache2 home directory set to '/var/www' or '/var/www/html'
+if [ -d '/var/www/html' ]; then
+	sudo ln -s `pwd` /var/www/html 
+else
+	sudo ln -s `pwd` /var/www
+fi
