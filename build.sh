0d8383ea5d1cda29bebd1c4fb0c0b0a8349fad67
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-26 15:52:28 +0530

    Changes made to optimise the code

--- build.sh
+++ build.sh
@@ -4,41 +4,33 @@
 path=`pwd`
 arch=$(uname -m)
 
+if [ $arch == 'i686' ]; then
+	TARGET_CPU='x86'
+elif [ $arch == 'armv61' ]; then
+	TARGET_CPU='arm'
+fi
+
 # building alljoyn component services
 printf "Building started for AllJoyn component services.\n"
 cd $path/components/alljoyn/core/alljoyn/
-if [ $arch == 'i686' ]; then
+if [ $TARGET_CPU == 'x86' ]; then
 	scons WS=off BINDINGS=cpp SERVICES="about,notification"
-
-	printf "Checking Alljoyn About Service . . . "
-	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/about/bin/AboutClient ]; then
-		printf "Build successful.!\n"
-	else
-		printf "\nSomething went wrong.!\n"
-	fi
-
-	printf "Checking Alljoyn notification Service . . . "
-	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/notification/bin/ConsumerService ] && [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/notification/bin/ProducerService ]; then
-		printf "Build successful.!\n"
-	else
-		printf "\nSomething went wrong.!\n"
-	fi
-elif [ $arch == 'armv61' ]; then
+elif [ $TARGET_CPU == 'arm' ]; then
 	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification"
+fi
 
-	printf "Checking Alljoyn About Service . . . "
-	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/about/bin/AboutClient ]; then
-		printf "Build successful.!\n"
-	else
-		printf "\nSomething went wrong.!\n"
-	fi
+printf "Checking Alljoyn About Service . . . "
+if [ -f $path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient ]; then
+	printf "Build successful.!\n"
+else
+	printf "\nSomething went wrong.!\n"
+fi
 
-	printf "Checking Alljoyn notification Service . . . "
-	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/notification/bin/ConsumerService ] && [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/notification/bin/ProducerService ]; then
-		printf "Build successful.!\n"
-	else
-		printf "\nSomething went wrong.!\n"
-	fi
+printf "Checking Alljoyn notification Service . . . "
+if [ -f $path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ConsumerService ] && [ -f $path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ProducerService ]; then
+	printf "Build successful.!\n"
+else
+	printf "\nSomething went wrong.!\n"
 fi
 
 cd $path
