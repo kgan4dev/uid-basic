c2836cbfc5e44f5bf74c3398de839b4632d63ef2
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-26 15:28:42 +0530

    Changes made to reduce number of scons

--- build.sh
+++ build.sh
@@ -6,46 +6,39 @@ arch=$(uname -m)
 
 # building alljoyn component services
 printf "Building started for AllJoyn component services.\n"
-cd $path/components/alljoyn/core/alljoyn/services/about/
+cd $path/components/alljoyn/core/alljoyn/
 if [ $arch == 'i686' ]; then
-	scons WS=off BINDINGS=cpp
+	scons WS=off BINDINGS=cpp SERVICES="about,notification"
 
-	if [ -f $path/components/alljoyn/core/alljoyn/services/about/build/linux/x86/debug/dist/about/bin/AboutClient ]; then
-		printf "Alljoyn About Service build successful.!\n"
+	printf "Checking Alljoyn About Service . . . "
+	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/about/bin/AboutClient ]; then
+		printf "Build successful.!\n"
 	else
-		printf "Something went wrong.!\n"
+		printf "\nSomething went wrong.!\n"
+	fi
+
+	printf "Checking Alljoyn notification Service . . . "
+	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/notification/bin/ConsumerService ] && [ -f $path/components/alljoyn/core/alljoyn/build/linux/x86/debug/dist/notification/bin/ProducerService ]; then
+		printf "Build successful.!\n"
+	else
+		printf "\nSomething went wrong.!\n"
 	fi
 elif [ $arch == 'armv61' ]; then
-	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr 
+	scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr SERVICES="about,notification"
 
-	if [ -f $path/components/alljoyn/core/alljoyn/services/about/build/linux/arm/debug/dist/about/bin/AboutClient ]; then
-		printf "Alljoyn About Service build successful.!\n"
+	printf "Checking Alljoyn About Service . . . "
+	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/about/bin/AboutClient ]; then
+		printf "Build successful.!\n"
 	else
-		printf "Something went wrong.!\n"
+		printf "\nSomething went wrong.!\n"
 	fi
-fi
 
-cd $path/components/alljoyn/services/base/notification/
-if [ $arch == 'i686' ]; then
-        scons WS=off BINDINGS=cpp
-	
-	cd build/linux/x86/debug/dist/about/bin
-
-        if [ -f ConsumerService ] && [  -f ProducerService ]; then
-                printf "Alljoyn Notification Service build successful.!\n"
-        else
-                printf "Something went wrong.!\n"
-        fi
-elif [ $arch == 'armv61' ]; then
-        scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr
-
-        cd build/linux/arm/debug/dist/about/bin
-
-        if [ -f ConsumerService ] && [  -f ProducerService ]; then
-                printf "Alljoyn About Service build successful.!\n"
-        else
-                printf "Something went wrong.!\n"
-        fi
+	printf "Checking Alljoyn notification Service . . . "
+	if [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/notification/bin/ConsumerService ] && [ -f $path/components/alljoyn/core/alljoyn/build/linux/arm/debug/dist/notification/bin/ProducerService ]; then
+		printf "Build successful.!\n"
+	else
+		printf "\nSomething went wrong.!\n"
+	fi
 fi
 
 cd $path
