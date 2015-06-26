854dc1dfcba1d084f4ea9f67b23d17431f5b37dc
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-26 15:01:15 +0530

    Changes made for arm to build and check now

--- build.sh
+++ build.sh
@@ -28,8 +28,10 @@ fi
 cd $path/components/alljoyn/services/base/notification/
 if [ $arch == 'i686' ]; then
         scons WS=off BINDINGS=cpp
+	
+	cd build/linux/x86/debug/dist/about/bin
 
-        if [ -f $path/components/alljoyn/services/base/notification/build/linux/x86/debug/dist/about/bin/ConsumerService ] && [  -f $path/components/alljoyn/services/base/notification/build/linux/x86/debug/dist/about/bin/ProducerService ]; then
+        if [ -f ConsumerService ] && [  -f ProducerService ]; then
                 printf "Alljoyn Notification Service build successful.!\n"
         else
                 printf "Something went wrong.!\n"
@@ -37,7 +39,9 @@ if [ $arch == 'i686' ]; then
 elif [ $arch == 'armv61' ]; then
         scons WS=off BINDINGS=cpp CPU=arm OE_BASE=/usr
 
-        if [ -f $path/components/alljoyn/core/alljoyn/services/about/build/linux/arm/debug/dist/about/bin/AboutClient ]; then
+        cd build/linux/arm/debug/dist/about/bin
+
+        if [ -f ConsumerService ] && [  -f ProducerService ]; then
                 printf "Alljoyn About Service build successful.!\n"
         else
                 printf "Something went wrong.!\n"
