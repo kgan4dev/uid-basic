1667030f49e0c20ad6ba5887c1768793639376bf
GANESH KAILA <kailaganesh07@gmail.com>
2015-06-29 11:16:11 +0530

    Service restart functionality added

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ path=`pwd`
 arch=$(uname -m)
 
 if [ -z "$arg" ]; then
-	printf "USAGE: ./run.sh {start/stop}\n"
+	printf "USAGE: ./run.sh {start/stop/restart}\n"
 	exit
 fi
 
@@ -19,29 +19,84 @@ fi
 if [ $arg == 'start' ]; then
 	export LD_LIBRARY_PATH=$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$LD_LIBRARY_PATH
 
-	printf "Starting Alljoyn component services . . . \n\n"
-	printf "About Client service . . . 		"
-	$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient >> $path/components/logs/alljoyn/AboutClient.log 2>&1 &
-	printf "Done\n"
-	printf "Notification Consumer service . . .	"
-	$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ConsumerService >> $path/components/logs/alljoyn/ConsumerService.log 2>&1 &
-	printf "Done\n\n"
+	printf "Starting Alljoyn component services . . . \n"
+
+	printf "\nAbout Client service 		\n"
+	if [ ! -z "$(ps | grep "AboutClient" | awk '{print $1}')" ];then
+		printf "Service already running. . . \n"
+	else
+		printf "Service Starting. . .                   "
+                $path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient >> $path/components/logs/alljoyn/AboutClient.log 2>&1 &
+                printf "Done\n"
+	fi
+
+	printf "\nNotification Consumer service	\n"
+	if [ ! -z "$(ps | grep "ConsumerService" | awk '{print $1}')" ];then
+		printf "Service already running. . . \n"
+	else
+		printf "Service Starting. . .                   "
+		$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ConsumerService >> $path/components/logs/alljoyn/ConsumerService.log 2>&1 &
+                printf "Done\n"
+	fi
+
+#	printf "Notification Producer service	"
+#	$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ProducerService >> $path/components/logs/alljoyn/ProducerService.log  2>&1 &
+#	printf "Done\n\n"
+	printf "\nAlljoyn component services started Successfully.!\n"
+elif [ $arg == 'restart' ];then
+	export LD_LIBRARY_PATH=$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/cpp/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/lib:$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/lib:$LD_LIBRARY_PATH
+
+	printf "Restarting Alljoyn component services . . . \n"
+
+	printf "\nAbout Client service 		\n"
+	if [ ! -z "$(ps | grep "AboutClient" | awk '{print $1}')" ];then
+		printf "Service Stopping. . .			"
+		kill -9 $(ps | grep "AboutClient" | awk '{print $1}')
+		printf "Done\n"
+		printf "Service Starting. . . 			"
+		$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient >> $path/components/logs/alljoyn/AboutClient.log 2>&1 &
+		printf "Done\n"
+	else
+		printf "Service Starting. . .                   "
+                $path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/about/bin/AboutClient >> $path/components/logs/alljoyn/AboutClient.log 2>&1 &
+                printf "Done\n"
+	fi
+
+	printf "\nNotification Consumer service	\n"
+	if [ ! -z "$(ps | grep "ConsumerService" | awk '{print $1}')" ];then
+		printf "Service Stopping. . .			"
+		kill -9 $(ps | grep "ConsumerService" | awk '{print $1}')
+		printf "Done\n"
+		printf "Service Starting. . . 			"
+		$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ConsumerService >> $path/components/logs/alljoyn/ConsumerService.log 2>&1 &
+		printf "Done\n"
+	else
+		printf "Service Starting. . .                   "
+		$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ConsumerService >> $path/components/logs/alljoyn/ConsumerService.log 2>&1 &
+                printf "Done\n"
+	fi
+
 #	printf "Notification Producer service . . .	"
 #	$path/components/alljoyn/core/alljoyn/build/linux/$TARGET_CPU/debug/dist/notification/bin/ProducerService >> $path/components/logs/alljoyn/ProducerService.log  2>&1 &
 #	printf "Done\n\n"
-	printf "Alljoyn component services started Successfully.!\n"
+	printf "\nAlljoyn component services restarted Successfully.!\n"
+	
 elif [ $arg == 'stop' ]; then
-	printf "Stoping Alljoyn component services . . . \n\n"	
-	printf "About Client service		"
+	printf "Stoping Alljoyn component services . . . \n"	
+
+	printf "\nAbout Client service\n"
+	printf "Service stopping. . .			"
 	kill -9 $(ps | grep "AboutClient" | awk '{print $1}')
 	printf "Done\n"
-	printf "Notification Consumer Service 	"
+
+	printf "\nNotification Consumer Service\n"
+	printf "Service stopping. . .			"
 	kill -9 $(ps | grep "ConsumerService" | awk '{print $1}')
-	printf "Done\n\n"
-#	printf "Notification Producer Service 	"
+	printf "Done\n"
+#	printf "Notification Producer Service. . . 	"
 #	kill -9 $(ps | grep "ProducerService" | awk '{print $1}')
 #	printf "Done\n\n"
-	printf "Alljoyn component services stop successfully.!\n"
+	printf "\nAlljoyn component services stop successfully.!\n"
 else
-	printf "USAGE: ./run.sh {start/stop}\n"
+	printf "USAGE: ./run.sh {start/stopi/restart}\n"
 fi
